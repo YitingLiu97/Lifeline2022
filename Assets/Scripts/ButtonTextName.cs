199c26d1c2013003cf0c11ea60c1ade424f79b5d
Yiting Liu <yiting@futurecolossal.com>
2022-03-25 11:46:59 -0400

    button and json save success

---
+++ Assets/Scripts/ButtonTextName.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class ButtonTextName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  GetComponentInParent<Button>().name = GetComponent<TextMeshProUGUI>().text; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
