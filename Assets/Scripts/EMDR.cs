c3fc6054042fce838db72fc0ac3bd065227422a4
Bryant Wood <bdw7383@gmail.com>
2022-03-26 22:38:06 -0400

    EMDR stub and moment move
    _assets is back, sorry

---
+++ Assets/Scripts/EMDR.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMDR : MonoBehaviour
+{
+    public Transform leftSide;
+    public Transform rightSide;
+    public GameObject audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
